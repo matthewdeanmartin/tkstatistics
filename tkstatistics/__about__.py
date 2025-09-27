8cc52d34eeede6e9822ff8c355c67de068a10a6b
Matthew Martin <matthewdeanmartin@gmail.com>
2025-09-27 09:10:14 -0400

    0.1.0: lock and about

---
+++ tkstatistics/__about__.py
@@ -0,0 +1,9 @@
+"""Metadata for tkstatistics."""
+
+__all__ = ["__title__", "__version__", "__description__", "__readme__", "__requires_python__"]
+
+__title__ = "tkstatistics"
+__version__ = "0.1.0"
+__description__ = "Statistics Application using only the Python Standard Library."
+__readme__ = "README.md"
+__requires_python__ = ">=3.13.2"
